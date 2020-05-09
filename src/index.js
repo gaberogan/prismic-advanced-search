ecea8d8ed57ad99c62bfd325c9e9449b7a394701
Gabe Rogan <rogangabe@gmail.com>
2020-05-09 11:26:42 +0100

    fill in title etc

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { html, render, useState, usePromise, useEventListener } from '/lib/react.js'
 import { PrismicClient } from '/lib/prismic.js'
 import { gql } from '/lib/graphql.js'
+import moment from '/lib/moment.js'
 
 const CUSTOM_TYPES_QUERY = gql`
 query types {
@@ -27,12 +28,13 @@ query inputs {
 }
 `
 
-const makeSearchQuery = (typeNames, variables) => gql`
+const makeSearchQuery = (types, variables) => gql`
 query search {
-    ${typeNames.map(t => `
-    ${t}(where: ${objToGqlVar(variables)}) {
+    ${types.map(t => `
+    ${t.type}(where: ${objToGqlVar(variables)}) {
         edges {
             node {
+                ${t.summary ? `summary: ${t.summary}` : ''}
                 _meta {
                     id
                     type
@@ -54,21 +56,31 @@ const parseQuery = query => {
 
 const objToGqlVar = obj => JSON.stringify(obj).replace(/"([^"]+)":/g, '$1:')
 
-const SearchBar = () => {
-    const [query, setQuery] = useState('')
-
-    const queryables = usePromise(async () => {
-        const typesResponse = await PrismicClient.query({ query: CUSTOM_TYPES_QUERY })
-        const types = typesResponse.data.__schema.types.map(t => t.name).filter(s => s.startsWith('Where'))
-        const inputsResponse = await PrismicClient.query({ query: makeInputsQuery(types) })
-        // map response to [{ type: allMy_docs, inputFields: [{ name: some_field_fulltext, type: String }] }]
-        return Object.entries(inputsResponse.data).map(entry => ({
+const getQueryables = async () => {
+    const typesResponse = await PrismicClient.query({ query: CUSTOM_TYPES_QUERY })
+    const types = typesResponse.data.__schema.types.map(t => t.name).filter(s => s.startsWith('Where'))
+    const inputsResponse = await PrismicClient.query({ query: makeInputsQuery(types) })
+    // map response to [{
+    //   type: allMy_docs,
+    //   summary: some_field, <-- for showing a text preview
+    //   inputFields: [{ name: some_field_fulltext, type: String }]
+    // }, ...]
+    return Object.entries(inputsResponse.data).map(entry => {
+        const stringFields = entry[1].inputFields.filter(f => f.type.name === 'String').map(f => f.name)
+        const fulltextFields = stringFields.filter(f => f.endsWith('_fulltext')).map(f => f.slice(0, -9))
+        return ({
             type: `all${entry[0].slice(5)}s`,
+            summary: fulltextFields[0] || stringFields[0] || null,
             inputFields: entry[1].inputFields
                 .map(f => ({ name: f.name, type: f.type.name }))
-                .filter(i => i.type === 'String'), // filter out non-string types
-        }))
+                .filter(f => f.type === 'String'), // filter out non-string types
+        })
     })
+}
+
+const SearchBar = () => {
+    const [query, setQuery] = useState('')
+    const queryables = usePromise(getQueryables)
 
     useEventListener('keydown', async e => {
         // only on press enter while input focused
@@ -84,24 +96,23 @@ const SearchBar = () => {
         const keys = Object.keys(variables)
 
         // ONLY types that have all the query fields asked for
-        const types = queryables.filter(q => keys.every(k => q.inputFields.some(i => i.name === k)))
-        const typeNames = types.map(t => t.type)
+        const types = queryables.filter(q => keys.every(k => q.inputFields.some(f => f.name === k)))
 
         // make request, transform response
-        const searchResponse = await PrismicClient.query({ query: makeSearchQuery(typeNames, variables) })
-        const metas = Object.values(searchResponse.data).map(t => t.edges.map(edge => edge.node._meta)).flat()
+        const searchResponse = await PrismicClient.query({ query: makeSearchQuery(types, variables) })
+        const nodes = Object.values(searchResponse.data).map(t => t.edges.map(edge => edge.node)).flat()
 
         // inject results into DOM
         const tbody = document.querySelector('#app > #viewport section#documents .items .versions-list table tbody')
-        tbody.innerHTML = metas.map(meta => `
-        <tr class="list__item" href="/documents~b=working&c=unclassified/${meta.id}/">
+        tbody.innerHTML = nodes.map(n => `
+        <tr class="list__item" href="/documents~b=working&c=unclassified/${n._meta.id}/">
             <td class="list__item-icon">
                 <span class="live">Live</span>
             </td>
-            <td class="list__item-title">TBD Title</td>
+            <td class="list__item-title">${n.summary || 'Document'}</td>
             <td class="list__item-translate"></td>
-            <td class="list__item-mask">tbd_type</td>
-            <td class="list__item-lastupdate">X minutes ago</td>
+            <td class="list__item-mask">${n._meta.type}</td>
+            <td class="list__item-lastupdate">${moment(n._meta.lastPublicationDate).fromNow()}</td>
             <td class="list__item-author">
                 <span class="md-avatar tarracotta" data-letter="a"></span>
             </td>
