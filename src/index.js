8f3627d5eac9ea88f41b91cb5167ff0e33f16290
Gabe Rogan <rogangabe@gmail.com>
2020-06-25 20:50:13 +0100

    bugfixes etc

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import {
     html,
-    render,
+    ReactDOM,
     useState,
     useEffect,
     usePromise,
-    useEventListener,
     useSyncStorage,
 } from '/lib/react.js'
 import { PrismicClient } from '/lib/prismic.js'
@@ -12,6 +11,8 @@ import { gql } from '/lib/graphql.js'
 import moment from '/lib/moment.js'
 import { SVG, saveSolid, timesCircleSolid } from '/lib/svg.js'
 import Input from '/lib/ui/input.js'
+
+const switchy = val => opts => opts[val]
 
 const CUSTOM_TYPES_QUERY = gql`
 query types {
@@ -102,11 +103,11 @@ const getQueryables = async () => {
     //   inputFields: [{ name: some_field_fulltext, type: String }]
     // }, ...]
     return Object.entries(inputsResponse.data).map(entry => {
-        const stringFields = entry[1].inputFields.filter(f => f.type.name === 'String').map(f => f.name)
-        const fulltextFields = stringFields.filter(f => f.endsWith('_fulltext')).map(f => f.slice(0, -9))
+        const stringFieldNames = entry[1].inputFields.filter(f => f.type.name === 'String').map(f => f.name)
+        const fulltextFieldNames = stringFieldNames.filter(f => f.endsWith('_fulltext')).map(f => f.slice(0, -9))
         return ({
             type: `all${entry[0].slice(5)}s`,
-            summary: fulltextFields[0] || stringFields[0] || null,
+            summary: fulltextFieldNames[0] || stringFieldNames[0] || null,
             inputFields: entry[1].inputFields
                 .map(f => ({ name: f.name, type: f.type.name }))
                 .filter(f => f.type === 'String'), // filter out non-string types
@@ -114,20 +115,58 @@ const getQueryables = async () => {
     })
 }
 
-const makeDocDOM = n => `
-<tr class="list__item" href="/documents~b=working&c=unclassified/${n._meta.id}/">
-    <td class="list__item-icon">
-        <span class="live">Live</span>
-    </td>
-    <td class="list__item-title">${n.summary || 'Document'}</td>
+const makeDocDOM = nodes => !nodes.length ? `
+<div class="empty-state">
+    <div class="empty-state__illustration empty-state__illustration--search"></div>
+    <h2>No content matching this request</h2>
+    <p> Try to search using other tags, types, authors or keyword.</p>
+</div>
+` : `
+<div class="versions-wrapper">
+<div class="versions-list">
+    <table>
+    <thead>
+    <tr>
+    <td class="list__item-icon">State</td>
+    <td class="list__item-title">Name</td>
     <td class="list__item-translate"></td>
-    <td class="list__item-mask">${n._meta.type}</td>
-    <td class="list__item-lastupdate">${moment(n._meta.lastPublicationDate).fromNow()}</td>
-    <td class="list__item-author">
-        <span class="md-avatar tarracotta" data-letter="a"></span>
-    </td>
-</tr>
+    <td class="list__item-mask">Type</td>
+    <td class="list__item-lastupdate">Last update</td>
+    <td class="list__item-author">Author</td>
+    </tr>
+    </thead>
+    </table>
+</div>
+<div class="versions-list">
+    <table>
+        <tbody>
+        ${nodes.map(n => `
+            <tr class="list__item" href="/documents~b=working&c=unclassified/${n._meta.id}/">
+                <td class="list__item-icon">
+                    <span class="live">Live</span>
+                </td>
+                <td class="list__item-title">${switchy(typeof n.summary || 0)({
+                    string: n.summary,
+                    object: n.summary[0].text,
+                    number: 'Document',
+                })}</td>
+                <td class="list__item-translate"></td>
+                <td class="list__item-mask">${n._meta.type}</td>
+                <td class="list__item-lastupdate">${moment(n._meta.lastPublicationDate).fromNow()}</td>
+                <td class="list__item-author">
+                    <span class="md-avatar tarracotta" data-letter="a"></span>
+                </td>
+            </tr>
+        `)}
+        </tbody>
+    </table>
+</div>
+</div>
 `
+
+let originalSearch = null
+
+const getReloadableEL = () => document.querySelector('#app>#viewport section#documents .items .reloadable')
 
 const searchForDocs = async (queryables, query) => {
     // need the schema to make the search request
@@ -149,22 +188,32 @@ const searchForDocs = async (queryables, query) => {
     const nodes = notypes ? [] : Object.values(searchResponse.data).map(t => t.edges.map(edge => edge.node)).flat()
 
     // inject results into DOM
-    const tbodyEl = document.querySelector('#app>#viewport section#documents .items .versions-list table tbody')
-    tbodyEl.innerHTML = nodes.map(makeDocDOM)
+    getReloadableEL().innerHTML = makeDocDOM(nodes)
 
     return true // it worked
 
-    // TODO info icon
+    // TODO uid/slug search
+    // TODO info icon (later)
+    // TODO queryable slug & empty input when url changes (later)
+    // TODO arrow thru autofill (later)
     // TODO handle error better (later)
-    // TODO empty the input when the url changes (later)
 }
 
 const SearchBar = () => {
     const [query, setQuery] = useState('')
+    const [placeholder, setPlaceholder] = useState('Advanced Search')
     const [namingQuery, setNamingQuery] = useState(false)
     const [lastQueried, setLastQueried] = useState('')
-    const queryables = usePromise(getQueryables)
+    const queryables = usePromise(getQueryables, [])
     const [savedQueries, setSavedQueries] = useSyncStorage('savedQueries', [])
+
+    // Autofill
+    const lastQuery = query.split(',').slice(-1)[0].trim()
+    const autofillSuggestions = [...new Set(queryables
+        .map(q => q.inputFields).flat().map(i => i.name.replace(/_fulltext$/, ''))
+        .filter(i => i.includes(lastQuery)))]
+    const showAutofill = Boolean(lastQuery && autofillSuggestions.length)
+    const setAutofill = str => setQuery(query.split(',').slice(0, -1).concat(str + ':').join(','))
 
     // Search
     const search = async (q = query) => {
@@ -186,7 +235,7 @@ const SearchBar = () => {
 
     // Forget search
     const forgetQuery = i => e => {
-        e.stopImmediatePropagation()
+        e.nativeEvent.stopImmediatePropagation()
         setSavedQueries(savedQueries.filter((_, idx) => i !== idx))
     }
 
@@ -198,14 +247,12 @@ const SearchBar = () => {
         firstTag.select()
     }, [namingQuery])
 
-    // Search on 'Enter'
-    useEventListener('keydown', async e => {
-        // only on press enter while input focused
-        if (e.keyCode !== 13) return
-        const el = document.querySelector('.advanced-search-input')
-        if (el !== document.activeElement) return
-        search()
-    })
+    // Search without need for keypress, save/restore original search
+    useEffect(() => {
+        if (!originalSearch) originalSearch = getReloadableEL().innerHTML
+        if (!query) getReloadableEL().innerHTML = originalSearch
+        if (query) search()
+    }, [query])
 
     return html`
         <span class=advanced-search-bar>
@@ -215,7 +262,9 @@ const SearchBar = () => {
                     type=text
                     value=${query}
                     onInput=${e => setQuery(e.target.value)}
-                    placeholder='Advanced Search'
+                    placeholder=${placeholder}
+                    onFocus=${() => setPlaceholder('first_name: Mark, last_name: Lee')}
+                    onBlur=${() => setPlaceholder('Advanced Search')}
                 />
                 <${SVG}
                     src=${saveSolid}
@@ -223,6 +272,13 @@ const SearchBar = () => {
                     onClick=${saveQuery({ name: 'New Search', value: query })}
                 />
             </span>
+            ${showAutofill && html`
+                <span class=advanced-search-autofill>
+                    ${autofillSuggestions.map(str => html`
+                        <span onClick=${() => setAutofill(str)}>${str}</span>
+                    `)}
+                </span>
+            `}
             ${savedQueries.map((q, i) => html`
                 <span
                     class=${`advanced-search-tag ${q.value === lastQueried ? 'selected' : ''}`}
@@ -253,5 +309,5 @@ export async function main() {
     const filtersEl = document.querySelector('#documents-filter-values')
     const root = document.createElement('div')
     controlsEl.insertBefore(root, filtersEl)
-    render(html`<${SearchBar} />`, root)
+    ReactDOM.render(html`<${SearchBar} />`, root)
 }
